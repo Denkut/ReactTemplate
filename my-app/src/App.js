39068d038e98432c8264e556f0dd87a7c8fcd0d8
IDenis <idenis@MacBook-Air-Denis.local>
2023-08-15 19:26:24 +0300

    Добавил валидную форму регистрации

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,43 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+/* eslint-disable */
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+//начальное состояние
+const initialState = {
+	email: '',
+	password: '',
+	confirmPassword: '',
+};
+
+const emailRegex =
+	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+//свой хук
+const useStore = () => {
+	const [state, setState] = useState(initialState);
+
+	return {
+		//возрващаем начальное состояние
+		getState: () => state,
+		//возвращаем обновленное состояние
+		updateState: (fieldName, newValue) => {
+			//передаем сначало начальное состояние(email, password, confirmPassword), потом новое состояние по имени[fieldName]
+			setState({ ...state, [fieldName]: newValue });
+			// console.log('updateState', state);
+		},
+	};
+};
+//передача в консоль
+const sendData = (formData) => {
+	console.log(formData);
+};
 
 export const App = () => {
-	const dateNow = new Date();
-	// императивный
-	return React.createElement(
-		'div',
-		{
-			className: 'App',
-		},
-		React.createElement(
-			'header',
-			{
-				className: 'App-header',
-			},
-			React.createElement('img', {
-				src: logo,
-				className: 'App-logo',
-				alt: 'logo',
-			}),
-			React.createElement(
-				'p',
-				null,
-				'Edit ',
-				React.createElement('code', null, 'src/App.js'),
-				' and save to reload.',
-			),
-			React.createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			/*#__PURE__*/ React.createElement('p', null, dateNow.getFullYear()),
-		),
+	const { getState, updateState } = useStore('');
+	const [errors, setErrors] = useState({});
+	const [validForm, setValidForm] = useState(false);
+
+	const validateValues = (inputValues) => {
+		let errors = {};
+		console.log('validateValues', inputValues);
+		if (inputValues.email.length > 0 && !emailRegex.test(inputValues.email)) {
+			errors.email = 'Некоректный Email';
+		}
+		if (inputValues.password.length > 0 && inputValues.password.length < 2) {
+			errors.password = 'Password is too short';
+		}
+		if (
+			inputValues.confirmPassword.length > 0 &&
+			inputValues.confirmPassword !== inputValues.password
+		) {
+			errors.confirmPassword = 'Пароль должен совпадать';
+		}
+		return errors;
+	};
+
+	const checkValidForm = () => {
+		const form = getState();
+		for (const key in form) {
+			if (form[key] === '') {
+				return false;
+			}
+			if (Object.keys(errors).length !== 0) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+	const onBlur = (e) => {};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		sendData(getState());
+	};
+	const { email, password, confirmPassword } = getState();
+
+	//передаем имя поля и новое значение
+	const onChange = (event) => {
+		updateState(event.target.name, event.target.value);
+	};
+
+	useEffect(() => {
+		const currentErrors = validateValues(getState());
+		setErrors(currentErrors);
+	}, [email, password, confirmPassword]);
+
+	useEffect(() => {
+		setValidForm(checkValidForm());
+	}, [email, password, confirmPassword, errors]);
+	return (
+		<div className={styles.app}>
+			<form className={styles.formInput} onSubmit={onSubmit}>
+				<h1>Регистрация</h1>
+				{errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
+
+				<input
+					className={styles.input}
+					type="email"
+					name="email"
+					value={email}
+					placeholder="Введите email"
+					onChange={onChange}
+					onBlur={onBlur}
+				/>
+				{errors.password && <div style={{ color: 'red' }}>{errors.password}</div>}
+				<input
+					className={styles.input}
+					type="password"
+					name="password"
+					value={password}
+					placeholder="Введите пароль"
+					onChange={onChange}
+					onBlur={onBlur}
+				/>
+				{errors.confirmPassword && (
+					<div style={{ color: 'red' }}>{errors.confirmPassword}</div>
+				)}
+				<input
+					className={styles.input}
+					type="password"
+					name="confirmPassword"
+					value={confirmPassword}
+					placeholder="Введите повторно пароль"
+					onChange={onChange}
+					onBlur={onBlur}
+				/>
+				<button disabled={!validForm} type="submit">
+					Зарегестрироваться
+				</button>
+			</form>
+		</div>
 	);
 };
